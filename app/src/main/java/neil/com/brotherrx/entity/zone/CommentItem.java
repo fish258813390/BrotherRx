d67399df75cf44685b1e4e6dd818ad1a9bf6b3d0
fish258813390 <258813390@qq.com>
2018-01-01 21:12:29 +0800

    1.点赞bean；

---
+++ app/src/main/java/neil/com/brotherrx/entity/zone/CommentItem.java
@@ -0,0 +1,167 @@
+package neil.com.brotherrx.entity.zone;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+/**
+ * 说说 里的评论bean
+ * Created by neil on 2017/12/31 0031.
+ */
+public class CommentItem implements Parcelable{
+
+    /**
+     "id":24,
+     "content":"肥死你",
+     "createTime":1471489658000,
+     "appointUserid":0,
+     "publishId":13,
+     "appointUserNickname":"",
+     "userId":10002,
+     "pictures":"",
+     "userNickname":"小鹏"
+     */
+
+    private String appointUserNickname;
+    private String appointUserid;
+    private String content; // 评论内容
+    private String createTime;
+    private String id;
+    private String pictures;
+    private String publishId; // 说说发布者id
+    private String userId; // 评论用户id
+    private String userNickname; // 用户名
+
+    public CommentItem() {
+    }
+
+    public CommentItem(String appointUserNickname, String appointUserid, String content, String publishId, String userId, String userNickname) {
+        this.appointUserNickname = appointUserNickname;
+        this.appointUserid = appointUserid;
+        this.content = content;
+        this.publishId = publishId;
+        this.userId = userId;
+        this.userNickname = userNickname;
+    }
+
+    public String getAppointUserNickname() {
+        return appointUserNickname;
+    }
+
+    public void setAppointUserNickname(String appointUserNickname) {
+        this.appointUserNickname = appointUserNickname;
+    }
+
+    public String getAppointUserid() {
+        return appointUserid;
+    }
+
+    public void setAppointUserid(String appointUserid) {
+        this.appointUserid = appointUserid;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(String createTime) {
+        this.createTime = createTime;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getPictures() {
+        return pictures;
+    }
+
+    public void setPictures(String pictures) {
+        this.pictures = pictures;
+    }
+
+    public String getPublishId() {
+        return publishId;
+    }
+
+    public void setPublishId(String publishId) {
+        this.publishId = publishId;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getUserNickname() {
+        return userNickname;
+    }
+
+    public void setUserNickname(String userNickname) {
+        this.userNickname = userNickname;
+    }
+
+
+
+    public static final Creator<CommentItem> CREATOR = new Creator<CommentItem>() {
+        @Override
+        public CommentItem createFromParcel(Parcel in) {
+            return new CommentItem(in);
+        }
+
+        @Override
+        public CommentItem[] newArray(int size) {
+            return new CommentItem[size];
+        }
+    };
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    // 将值写入Parcel 中
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(this.appointUserNickname);
+        dest.writeString(this.appointUserid);
+        dest.writeString(this.content);
+        dest.writeString(this.createTime);
+        dest.writeString(this.id);
+        dest.writeString(this.pictures);
+        dest.writeString(this.publishId);
+        dest.writeString(this.userId);
+        dest.writeString(this.userNickname);
+    }
+
+    /**
+     * 这里读的顺序必须与writeToParcel(Parcel dest,int flags) 方法中写的顺序保持一致
+     * 否则数据会有差错,导致读取的属性值不对
+     * @param source
+     */
+    protected CommentItem(Parcel source) {
+        appointUserNickname = source.readString();
+        appointUserid = source.readString();
+        content = source.readString();
+        createTime = source.readString();
+        id = source.readString();
+        pictures = source.readString();
+        publishId = source.readString();
+        userId = source.readString();
+        userNickname = source.readString();
+    }
+}
---
+++ app/src/main/java/neil/com/brotherrx/entity/zone/FavortItem.java
@@ -0,0 +1,113 @@
+package neil.com.brotherrx.entity.zone;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+/**
+ * 点赞分类
+ * Created by neil on 2017/12/31 0031.
+ */
+public class FavortItem implements Parcelable {
+
+    /**
+     * {
+     * "id":11,
+     * "createTime":1471406833000,
+     * "publishId":13,
+     * "userId":10000,
+     * "userNickname":"锋"
+     * }
+     */
+
+    private String createTime;
+    private String id;
+    private String publishId;
+    private String userId;
+    private String userNickname;
+
+
+
+
+    public static final Creator<FavortItem> CREATOR = new Creator<FavortItem>() {
+        @Override
+        public FavortItem createFromParcel(Parcel in) {
+            return new FavortItem(in);
+        }
+
+        @Override
+        public FavortItem[] newArray(int size) {
+            return new FavortItem[size];
+        }
+    };
+
+    public String getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(String createTime) {
+        this.createTime = createTime;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getPublishId() {
+        return publishId;
+    }
+
+    public void setPublishId(String publishId) {
+        this.publishId = publishId;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getUserNickname() {
+        return userNickname;
+    }
+
+    public void setUserNickname(String userNickname) {
+        this.userNickname = userNickname;
+    }
+
+    public FavortItem() {
+    }
+
+    public FavortItem(String publishId, String userId, String userNickname) {
+        this.publishId = publishId;
+        this.userId = userId;
+        this.userNickname = userNickname;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(this.createTime);
+        dest.writeString(this.id);
+        dest.writeString(this.publishId);
+        dest.writeString(this.userId);
+        dest.writeString(this.userNickname);
+    }
+
+    protected FavortItem(Parcel in) {
+        createTime = in.readString();
+        id = in.readString();
+        publishId = in.readString();
+        userId = in.readString();
+        userNickname = in.readString();
+    }
+}
