ac52f15854ce0793829be99c366ad16618137e49
fish258813390 <258813390@qq.com>
2018-01-22 22:26:37 +0800

    1.完善动态 评论及内容展示接口； 2.待完成 页面数据回调及展示

--- app/src/main/java/neil/com/brotherrx/ui/zone/activity/CircleZoneActivity.java
+++ app/src/main/java/neil/com/brotherrx/ui/zone/activity/CircleZoneActivity.java
@@ -2,11 +2,18 @@ package neil.com.brotherrx.ui.zone.activity;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Rect;
 import android.os.Handler;
 import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.view.ViewConfiguration;
+import android.view.ViewTreeObserver;
+import android.widget.LinearLayout;
 
 import com.aspsine.irecyclerview.IRecyclerView;
+import com.aspsine.irecyclerview.OnLoadMoreListener;
+import com.aspsine.irecyclerview.OnRefreshListener;
 import com.aspsine.irecyclerview.animation.ScaleInAnimation;
 import com.aspsine.irecyclerview.bean.PageBean;
 import com.aspsine.irecyclerview.widget.LoadMoreFooterView;
@@ -17,6 +24,7 @@ import com.jaydenxiao.common.commonwidget.LoadingTip;
 import java.util.List;
 
 import butterknife.BindView;
+import neil.com.baseretrofitrx.utils.LogUtils;
 import neil.com.brotherrx.R;
 import neil.com.brotherrx.ui.zone.adapter.CircleAdapter;
 import neil.com.brotherrx.ui.zone.bean.CircleItem;
@@ -38,6 +46,8 @@ public class CircleZoneActivity extends BaseActivity<CircleZonePresenter, ZoneMo
     IRecyclerView irc;
     @BindView(R.id.loadedTip)
     LoadingTip loadedTip;
+    @BindView(R.id.editTextBodyLl)
+    LinearLayout editTextBodyLl;
 
     private CircleAdapter mAdapter; // 评论
     private CommentConfig mCommentConfig;
@@ -77,10 +87,88 @@ public class CircleZoneActivity extends BaseActivity<CircleZonePresenter, ZoneMo
         irc.setLayoutManager(linearLayoutManager);
         irc.setAdapter(mAdapter);
 
+        // 监听recyclerview滑动
+        setViewTreeObserver();
+
+        // 上拉刷新
+        irc.setOnRefreshListener(new OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                mAdapter.getPageBean().setRefresh(true);
+                loadData();
+            }
+        });
+
+        irc.setOnLoadMoreListener(new OnLoadMoreListener() {
+            @Override
+            public void onLoadMore(final View loadMoreView) {
+                irc.setLoadMoreStatus(LoadMoreFooterView.Status.LOADING);
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        mAdapter.getPageBean().setRefresh(false);
+                        loadData();
+                    }
+                }, 1000);
+            }
+        });
+
+        // 监听列表滑动
+        irc.addOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
+                boolean b = Math.abs(dy) > ViewConfiguration.getTouchSlop();
+
+            }
+        });
 
         // 首次加载数据
         loadData();
     }
+
+    private void setViewTreeObserver() {
+        ViewTreeObserver swipeRefreshLayoutVTO = irc.getViewTreeObserver();
+        swipeRefreshLayoutVTO.addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+            @Override
+            public void onGlobalLayout() {
+                Rect r = new Rect();
+                irc.getWindowVisibleDisplayFrame(r);
+                int statusBarH = getStatusBarHeight();// 状态栏高度
+                int screenH = irc.getRootView().getHeight(); // 获取recyclerView所在容器的高度
+                if (r.top != statusBarH) {
+                    // r.top代表的是状态栏高度，在沉浸式状态栏时r.top＝0，通过getStatusBarHeight获取状态栏高度
+                    r.top = statusBarH;
+                }
+                int keyboardH = screenH - (r.bottom - r.top);
+                LogUtils.d("screenH＝ " + screenH + " &keyboardH = " + keyboardH + " &r.bottom=" + r.bottom + " &top=" + r.top + " &statusBarH=" + statusBarH);
+                if (keyboardH == mCurrentKeyboardH) {
+                    // 有变化才处理
+                    return;
+                }
+                mCurrentKeyboardH = keyboardH;
+                mScreenHeight = screenH;
+                mEditTextBodyHeight = editTextBodyLl.getHeight();
+
+                // listview偏移
+                if (irc != null && mCommentConfig != null) {
+                    int index = mCommentConfig.circlePosition + irc.getHeaderContainer().getChildCount() + 1;
+                    linearLayoutManager.scrollToPositionWithOffset(index,0);
+                }
+            }
+        });
+
+
+    }
+
+    // 测量偏移量
+    private int getListviewOffset(CommentConfig commentConfig){
+        if(commentConfig == null){
+            return 0;
+        }
+        // 如果listview上面还有其他占高度的控件,则需要减去该控件的高度,listview的headview除外。
+        return 0;
+    }
+
 
     private void loadData() {
         new Handler().postDelayed(new Runnable() {
@@ -139,7 +227,7 @@ public class CircleZoneActivity extends BaseActivity<CircleZonePresenter, ZoneMo
             // 不可加载
             irc.setLoadMoreStatus(LoadMoreFooterView.Status.THE_END);
         }
-        if(mAdapter.getData().size() > 0){
+        if (mAdapter.getData().size() > 0) {
             loadedTip.setLoadingTip(LoadingTip.LoadStatus.finish);
         } else {
             loadedTip.setLoadingTip(LoadingTip.LoadStatus.empty);
@@ -170,6 +258,16 @@ public class CircleZoneActivity extends BaseActivity<CircleZonePresenter, ZoneMo
     @Override
     public void updateEditTextBodyVisible(int visibility, CommentConfig commentConfig) {
 
+    }
+
+    // 获得状态栏高度
+    private int getStatusBarHeight() {
+        int result = 0;
+        int resourceId = getResources().getIdentifier("status_bar_height", "dimen", "android");
+        if (resourceId > 0) {
+            result = getResources().getDimensionPixelSize(resourceId);
+        }
+        return result;
     }
 
 
